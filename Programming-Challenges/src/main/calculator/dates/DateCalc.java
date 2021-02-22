9580fee789c4dbc3895a8b28098e32f982cb38b0
Marcin Jastrzebski <marcin.jastrzebski@outlook.com>
2021-02-22 12:23:48 +0100

    -#23 overrided Menu methods in DateCalc

--- Programming-Challenges/src/main/calculator/dates/DateCalc.java
+++ Programming-Challenges/src/main/calculator/dates/DateCalc.java
@@ -15,8 +15,23 @@ public class DateCalc implements Menu {
 
 	@Override
 	public void show() {
-		// TODO Auto-generated method stub
-		System.out.println("DateCalc");
+		System.out.println("Duration between two dates:");
+		System.out.print("Type the first date: (dd-MM-yyyy) : ");
+		final Scanner keyboard = new Scanner(System.in);
+		final String firstDate = keyboard.next();
+		final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+		final LocalDate startDate = LocalDate.parse(firstDate, formatter);
+		System.out.print("Type the second date: (dd-MM-yyyy) : ");
+		final String secondDate = keyboard.next();
+		final LocalDate finishDate = LocalDate.parse(secondDate, formatter);
+		System.out.println("The difference between is " + calculateDaysBetween(startDate, finishDate) + " day/days");
+		keyboard.close();
+	}
+
+	@Override
+	public void showDescription() {
+		System.out.print(this.getClass()
+				+ " is obliged to show you a difference between 2 dates which you use in calculateDaysBetween(final LocalDate startDate, final LocalDate finishDate) method");
 	}
 
 }
