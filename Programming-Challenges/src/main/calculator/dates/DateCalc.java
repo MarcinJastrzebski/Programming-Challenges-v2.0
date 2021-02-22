64d6e9b5a7f544e9da0f4fe8226efc63111bb3a2
Marcin Jastrzebski <marcin.jastrzebski@outlook.com>
2021-02-22 12:18:38 +0100

    -#23 added 2 methods to calculate days between

--- Programming-Challenges/src/main/calculator/dates/DateCalc.java
+++ Programming-Challenges/src/main/calculator/dates/DateCalc.java
@@ -2,7 +2,16 @@ package main.calculator.dates;
 
 import main.menu.Menu;
 
-public class DateCalc implements Menu{
+//#23
+public class DateCalc implements Menu {
+
+	public long calculateDaysBetween(final LocalDate startDate, final LocalDate finishDate) {
+		return ChronoUnit.DAYS.between(startDate, finishDate);
+	}
+
+	public long calculateDaysBetween(final LocalDateTime startDate, final LocalDateTime finishDate) {
+		return Duration.between(startDate, finishDate).toDays();
+	}
 
 	@Override
 	public void show() {
