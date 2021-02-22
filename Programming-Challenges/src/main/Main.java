45f074a1f6055496d19a0381eb64500781ba4dba
Marcin Jastrzebski <marcin.jastrzebski@outlook.com>
2021-02-22 12:28:20 +0100

    -#23 updated main method in the Main class

--- Programming-Challenges/src/main/Main.java
+++ Programming-Challenges/src/main/Main.java
@@ -1,13 +1,22 @@
 package main;
 
+import java.util.Scanner;
+
 import main.calculator.dates.DateCalc;
 import main.menu.Menu;
 import main.text.morse.MorseGenerator;
 
 public class Main {
-	
+
 	public static void main(String[] args) {
-		showMenu(new MorseGenerator());
+		showOptions();
+		Scanner keyboard = new Scanner(System.in);
+		String option;
+		do {
+			option = keyboard.next();
+			showMenu(option);
+		} while (!option.equals("q"));
+
 	}
 
 	public static void showMenu(String option) {
