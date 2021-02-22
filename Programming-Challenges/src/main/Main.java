f5bdb0e790b8acc02935acca6a881a3f19f783b0
Marcin Jastrzebski <marcin.jastrzebski@outlook.com>
2021-02-22 12:27:24 +0100

    -#23 added showing methods to the main class

--- Programming-Challenges/src/main/Main.java
+++ Programming-Challenges/src/main/Main.java
@@ -9,9 +9,26 @@ public class Main {
 	public static void main(String[] args) {
 		showMenu(new MorseGenerator());
 	}
-	
-	
-	public static void showMenu(Menu menu) {
-		menu.show();
+
+	public static void showMenu(String option) {
+		switch (option) {
+		case "23":
+			new DateCalc().show();
+			break;
+		case "q":
+			System.out.println("Quiting the program");
+			break;
+		default:
+			System.out.println("You didn't choose any available option");
+			break;
+		}
+	}
+
+	private static void showOptions() {
+		System.out.println("Programming-Challenges-v2.0");
+		System.out.println("Available options: ");
+		System.out.println("#23 Calculate the days between two dates");
+		System.out.println("");
+		System.out.println("Choose one of available options or press q and confirm");
 	}
 }
